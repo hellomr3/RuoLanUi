a6a2073b149e45e83e2a45af2f614c211bbf480f
guoqingshan <1072818264@qq.com>
2024-06-14 22:11:03 +0800

    feat: 1、新增发布kmp库gradle

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinMultiplatform
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
@@ -9,6 +12,7 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 kotlin {
@@ -110,6 +114,61 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "com.looptry.ui"
             packageVersion = "1.0.0"
+        }
+    }
+}
+
+/**
+ * 发布依赖到中央仓库
+ */
+mavenPublishing {
+    coordinates("io.github.hellomr3", "RuoLanUI", "0.0.1")
+    // sources publishing is always enabled by the Kotlin Multiplatform plugin
+    configure(
+        KotlinMultiplatform(
+            // configures the -javadoc artifact, possible values:
+            // - `JavadocJar.None()` don't publish this artifact
+            // - `JavadocJar.Empty()` publish an emprt jar
+            // - `JavadocJar.Dokka("dokkaHtml")` when using Kotlin with Dokka, where `dokkaHtml` is the name of the Dokka task that should be used as input
+            javadocJar = JavadocJar.Empty(),
+            // whether to publish a sources jar
+            sourcesJar = true,
+            // configure which Android library variants to publish if this project has an Android target
+            // defaults to "release" when using the main plugin and nothing for the base plugin
+            androidVariantsToPublish = listOf("debug", "release"),
+        )
+    )
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+//    // or when publishing to https://s01.oss.sonatype.org
+//    publishToMavenCentral(SonatypeHost.S01)
+//    // or when publishing to https://central.sonatype.com/
+//    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    pom {
+        name.set("RuoLanUI")
+        description.set("a kotlin mutiplatform common UI")
+        inceptionYear.set("2024")
+        url.set("https://github.com/hellomr3/RuoLanUI/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("hellomr3")
+                name.set("hellomr3")
+                url.set("https://github.com/hellomr3")
+            }
+        }
+        scm {
+            url.set("https://github.com/hellomr3/RuoLanUI/")
+            connection.set("scm:git:git://github.com/hellomr3/RuoLanUI.git")
+            developerConnection.set("scm:git:ssh://git@github.com/hellomr3/RuoLanUI.git")
         }
     }
 }
