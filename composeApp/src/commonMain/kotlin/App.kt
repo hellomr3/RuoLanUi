b81e6c1fcf45263075374dda02267caa845f23f2
guoqingshan <1072818264@qq.com>
2024-07-05 16:01:47 +0800

    feat: 1、完善示例

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,21 +1,55 @@
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import com.looptry.btn.BlockButton
+import com.ui.dialog.DialogScreen
 import com.ui.picker.PickerScreen
+import com.ui.toast.ToastScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-//    ToastScreen()
+    val nav = rememberNavController()
     MaterialTheme(
         colorScheme = if (isSystemInDarkTheme()) darkScheme else lightScheme
     ) {
-//        DialogScreen()
-        PickerScreen()
+        NavHost(navController = nav, startDestination = "/home") {
+            composable("/home") {
+                Home(nav2Home = { nav.navigate("/toast") },
+                    nav2Dialog = {
+                        nav.navigate("/dialog")
+                    }, nav2Picker = {
+                        nav.navigate("/picker")
+                    })
+            }
+            composable("/toast") {
+                ToastScreen()
+            }
+            composable("/dialog") {
+                DialogScreen()
+            }
+            composable("/picker") {
+                PickerScreen()
+            }
+        }
     }
 
+}
+
+@Composable
+fun Home(nav2Home: () -> Unit, nav2Dialog: () -> Unit, nav2Picker: () -> Unit) {
+    Column {
+        BlockButton(leadTitle = "Toast", onItemClick = nav2Home)
+        BlockButton(leadTitle = "Dialog", onItemClick = nav2Dialog)
+        BlockButton(leadTitle = "Picker", onItemClick = nav2Picker)
+    }
 }
 
 
